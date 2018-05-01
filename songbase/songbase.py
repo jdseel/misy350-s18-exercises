ef9e133947cfd62524563bfb4c28e752beb79d22
jdseel <seel@udel.edu>
2018-05-01 10:37:08 -0400

    flask2

--- songbase/songbase.py
+++ songbase/songbase.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 @app.route('/user')
 def user():
     return "Users page!"
 
+@app.route('/user/<string:username>')
+def users(username):
+    #return "<h1>Hello %s</h1>"% username
+    return render_template('user.html', uname=username)
+
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=4000)
+    app.run(debug=True)
