ea7ba62b3024f9858cd5e9ac1031ff0f2b0fae66
jdseel <seel@udel.edu>
2018-05-06 19:49:12 -0400

    myapp commit

--- songbase/songbase.py
+++ songbase/songbase.py
@@ -15,6 +15,16 @@ def users(username):
     #return "<h1>Hello %s</h1>"% username
     return render_template('user.html', uname=username)
 
+@app.route('/form')
+def form():
+    return render_template('form-basics.html', )
+
+@app.route('/form-demo')
+def form_demo():
+    if request.method == 'GET':
+        first_name = reuquest.args.get('first_name')
+        return first_name
+
 
 
 if __name__ == '__main__':
